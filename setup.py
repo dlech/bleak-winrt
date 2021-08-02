9a096081b07ceb58a584853d305fb6179feb1493
David Lechner <david@lechnology.com>
2021-08-01 19:44:18 -0500

    v1.0.0b5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as f:
 
 setup(
     name="bleak-winrt",
-    version = "1.0.0b4",
+    version = "1.0.0b5",
     description="Python WinRT bindings for Bleak",
     long_description=long_description,
     long_description_content_type="text/markdown",
